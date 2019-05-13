6ab5385ed9576f2a81a13ae28d2f379cc25e1814
Kareem Mohamed <kareem3d.a@gmail.com>
2019-05-13 20:22:51 +0200

    feat: server socket to accept multiple connections

--- src/com/bitriddler/tictactoe/Main.java
+++ src/com/bitriddler/tictactoe/Main.java
@@ -1,7 +1,19 @@
 package com.bitriddler.tictactoe;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("tic tac toe v2");
+        try (ServerSocket serverSocket = new ServerSocket(9991)) {
+            while (true) {
+                Socket connectionSocket = serverSocket.accept();
+                Runnable connectionHandler = new ConnectionHandler(connectionSocket);
+                new Thread(connectionHandler).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
